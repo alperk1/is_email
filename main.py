4df42e6e21d9c6a5a0a197a65dfe088f659ffff3
alperk1 <alpergenel@gmail.com>
2022-06-23 22:43:05 +0300

    valid invalid önüne email ekledik

--- main.py
+++ main.py
@@ -4,11 +4,8 @@ if __name__ == '__main__':
     print("deneme")
     regex = r"[a-z1-9_-]+@[a-z]+\.[a-z]+\.*[a-z]+"
     email_str = input("Please enter your e-mail: ")
-    print(email_str)
 
     if re.match(regex, email_str):
-        print("Valid")
+        print(email_str + " is valid")
     else:
-        print("Invalid")
-
-
+        print(email_str + " is invalid")
